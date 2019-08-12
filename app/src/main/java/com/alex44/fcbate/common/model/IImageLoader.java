449978b2fdb00076c7ee1997848dc71c5e450c70
Alexandr44 <alexandrgluhov@mail.ru>
2019-08-12 18:36:11 +0500

    feat: метод для Glide с crop

--- app/src/main/java/com/alex44/fcbate/common/model/IImageLoader.java
+++ app/src/main/java/com/alex44/fcbate/common/model/IImageLoader.java
@@ -1,5 +1,10 @@
 package com.alex44.fcbate.common.model;
 
 public interface IImageLoader<T> {
+
     void loadInto(String url, T container, int corners);
+
+    void loadInto(String url, T container);
+
+    void loadIntoWithCrop(String url, T container, int corners);
 }
--- app/src/main/java/com/alex44/fcbate/common/ui/GlideImageLoader.java
+++ app/src/main/java/com/alex44/fcbate/common/ui/GlideImageLoader.java
@@ -6,34 +6,35 @@ import android.widget.ImageView;
 import com.alex44.fcbate.common.model.IImageLoader;
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.MultiTransformation;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 
 public class GlideImageLoader implements IImageLoader<ImageView> {
 
-//    @Override
-//    public void loadInto(String url, ImageView container) {
-//        Glide.with(container.getContext())
-//                .asBitmap()
-//                .load(url)
-//                .listener(new RequestListener<Bitmap>() {
-//                    @Override
-//                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Bitmap> target, boolean isFirstResource) {
-//                        return false;
-//                    }
-//
-//                    @Override
-//                    public boolean onResourceReady(Bitmap resource, Object model, Target<Bitmap> target, DataSource dataSource, boolean isFirstResource) {
-//                        return false;
-//                    }
-//                })
-//                .into(container);
-//    }
-
     @Override
     public void loadInto(String url, ImageView container, int corners) {
         final MultiTransformation<Bitmap> multi = new MultiTransformation<>(
                 new RoundedCorners(corners)
+        );
+
+        Glide.with(container.getContext())
+                .load(url)
+                .apply(RequestOptions.bitmapTransform(multi))
+                .into(container);
+    }
+
+    @Override
+    public void loadInto(String url, ImageView container) {
+        Glide.with(container.getContext())
+                .load(url)
+                .into(container);
+    }
+
+    @Override
+    public void loadIntoWithCrop(String url, ImageView container, int corners) {
+        final MultiTransformation<Bitmap> multi = new MultiTransformation<>(
+                new CenterCrop(), new RoundedCorners(corners)
         );
 
         Glide.with(container.getContext())
